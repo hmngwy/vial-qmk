488ccf064c7d5dd801378457f2eb04db6f01fa13
hmngwy <1647347+hmngwy@users.noreply.github.com>
2022-04-24 13:22:10 +0800

    feat: add fifi, crkbd

---
+++ keyboards/crkbd/keymaps/fifi/rules.mk
@@ -0,0 +1,11 @@
+MOUSEKEY_ENABLE     = yes
+WPM_ENABLE	        = yes
+RGBLIGHT_ENABLE     = no     # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes     # Enable VIA
+VIAL_ENABLE         = yes     # Enable VIAL
+OLED_ENABLE         = yes
+OLED_DRIVER         = SSD1306
+EXTRAKEY_ENABLE     = yes
+LTO_ENABLE          = yes
+QMK_SETTINGS        = no
+COMBO_ENABLE        = no
